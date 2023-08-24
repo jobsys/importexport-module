1ce2bdc21209797c03687fe416fcb3c2507d8db8
sinceow <sinceow@163.com>
2023-08-24 15:56:14 +0800

    Upgrade

--- Routes/web.php
+++ Routes/web.php
@@ -10,7 +10,3 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::prefix('importexport')->group(function() {
-    Route::get('/', 'ImportExportController@index');
-});
